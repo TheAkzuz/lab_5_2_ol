556cf787929c90a3dcb6672014dee05df65b84a3
Oleg Kozlov <131234047+TheAkzuz@users.noreply.github.com>
2023-12-21 22:52:50 +0300

    Add files via upload

---
+++ lab_5_4_2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace lab_5_4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // открытие файла текстового
+            StreamReader sr;
+
+            try
+            {
+                 sr = new StreamReader("D:\\numbers.txt");
+            }
+
+            catch
+            { 
+                sr = new StreamReader("C:\\numbers.txt"); // у меня нету диска Д, поэтому сделал тру,катч блоки
+            
+            }
+
+
+            string line;
+            //тут делаем  коллекции, где негатив это гласные, а позив согласные
+            Queue negativ_n = new Queue();
+            //
+            Queue positiv_n = new Queue();
+
+            int k = 0;
+
+
+            do //это цыкл, где мы смотрим на строки. И если там гласнаые, то вставляем их в негатив и наоборот
+            {
+                
+                line = sr.ReadLine();
+                string gg = line;
+                //тут мы смо
+                if (line[0] == 'а' || line[0] == 'у' || line[0] == 'е' || line[0] == 'ы' || line[0] == 'о' || line[0] == 'э' || line[0] == 'я' || line[0] == 'ю' || line[0] == 'и' || line[0] == 'ё')
+              
+                 {
+                    negativ_n.Enqueue(line);
+                }
+                else 
+                {
+                    positiv_n.Enqueue(line);
+                }
+                k++;
+               
+            } while (line != null) ;
+
+            sr.Close(); //закрываем файл
+
+            //           Console.WriteLine(positiv_n.Count.ToString());
+            //     Console.ReadLine();
+            for (int i = 0; i<k-1; i++)
+            {
+                //тут ввыводим наш результат
+                if (negativ_n.Count != 0)
+                {
+                    positiv_n.Enqueue(negativ_n.Dequeue());
+                }
+                Console.WriteLine(positiv_n.Dequeue().ToString());
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ lab_5_4_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lab_5_4_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("lab_5_4_2")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c13de3db-ad6b-4294-841d-38c729951cd5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
